2698631a8c882427948ae219f028865eaa1d875f
Bruno Melo <brunousml@gmail.com>
2015-09-05 04:04:45 -0300

    remove dev to cart

--- script/main.js
+++ script/main.js
@@ -1,8 +1,15 @@
 $(window).load(function() {
 	$('button').click(function(){ // TODO: To better code move to template
 		addToCart($(this));
-		sumTotalCart();	    
+		sumTotalCart();
+
+		$('.remove').click(function(){ // TODO: To better code move to template
+			removeFromCart($(this));
+			sumTotalCart();	    
+	  	});	    
   	});
+
+  	
 });
 
 function addToCart(el){
@@ -10,12 +17,25 @@ function addToCart(el){
   					"<td>" + 
   						el.data("username") + 
 					"</td>" + 
-					"<td>$" + el.data("price") + "</td>"+
-					"<td><button class='btn btn-danger pull-right'>Remove</button></td></tr>" 
+					"<td class='price' data-price="+ el.data("price") +">$" + el.data("price") + "</td>"+
+					"<td><button class='btn btn-danger pull-right remove'>Remove</button></td></tr>" 
   	
   	$('#cart > tbody:last-child').append(html);
 }
 
 function sumTotalCart(){
+	var prices = $('#cart td.price');
+	var sum = 0;
+	for (var i = prices.length - 1; i >= 0; i--) {
+		sum = sum + parseInt(prices[i].dataset['price']);
+	};
+	updateTotalCart(sum);
+}
 
+function updateTotalCart(value){
+	$('#total-cart').html("$" + value);
+}
+
+function removeFromCart(el){
+	$(el).closest('tr').remove();
 }
